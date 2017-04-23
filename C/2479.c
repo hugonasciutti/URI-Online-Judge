6c32aa2a96870b845944968e85df97c9f03649c3
Hugo Nasciutti <hugonasciutti@gmail.com>
2017-04-23 17:19:01 -0300

    60p 2479

--- C/2479.c
+++ C/2479.c
@@ -18,18 +18,21 @@ void showAll(childList *l, childList i);
 
 int const MAXNOME = 20;
 int position = 0; // Position of the list, this const is incremented whenever a child is inserted in my list by the function insert
-int N = 0; // Number of child
+int N = 0; // Number of child inserted by the user
+int well_behaved = 0;
+int misbehaved = 0;
 
 int
 main() {
   // The first value N (0 ≤ N ≤ 100)
   childList i; // This is my structure, here i'm going to insert my child data.
   childList *l; // This is my structure pointer, this is where i'm going to walk in memory through increment later on the code, e.g.: l[position++].
-
   int j = 0;
 
-  while(N <= 0 || N > 100) {
-    scanf("%d", &N); // Ask the number of child
+  scanf("%d", &N); // Ask the number of child
+
+  while (N < 0 || N > 100) {
+    scanf("%d", &N); // Keep asking the number until meet the criteria
   }
 
   l = create(N); // Allocate a space in memory according to the number of childs in the list
@@ -43,13 +46,15 @@ main() {
   for (j = 0; j < N; j++)
   {
     setbuf(stdin, NULL);
-    i.name = (char *) malloc (MAXNOME * sizeof(char) + 1);
-    scanf ("%c %s", (char *)&i.behavior, i.name);
-
+    i.name = (char *) malloc (MAXNOME * sizeof(char));
+    scanf ("%c %s", &i.behavior, i.name);
+    while((unsigned)strlen(i.name) > MAXNOME) {
+      scanf ("%s", i.name);
+    }
     insert (l, i); // Walking through memory by this function filling the allocation with i;
   }
 
-
+  // In showAll it will sort and print in Alphabetical order
   showAll(l, i);
 
   return 0;
@@ -60,6 +65,7 @@ create(int N)
 {
   childList *l;
   l = (childList *) malloc(N * sizeof(childList));
+  return l;
 }
 
 int
@@ -69,13 +75,33 @@ insert(childList *l, childList i)
     return -1;
   }
 
+  if (i.behavior == '+') {
+    well_behaved++;
+  } else {
+    misbehaved++;
+  }
+
   l[position++] = i;
   return position;
 }
 
 void showAll(childList *l, childList i) {
-  int j = 0;
+  int j, k, check;
+  char * strTemp;
+  strTemp = malloc(MAXNOME * sizeof(char) + 1);
   for (j = 0; j < N; j++) {
-    printf("%c %s[%d]\n", l[j].behavior, l[j].name, j);
+    for (k = 0; k < N; k++) {
+      check = strcmp(l[j].name, l[k].name);
+      if (check<0) {
+        strcpy(strTemp, l[j].name);
+        strcpy(l[j].name, l[k].name);
+        strcpy(l[k].name, strTemp);
+      }
+    }
   }
+  for (j = 0; j < N; j++) {
+    printf("%s\n", l[j].name);
+  }
+
+  printf("Se comportaram: %d | Nao se comportaram: %d\n", well_behaved, misbehaved);
 }
